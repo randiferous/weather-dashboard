a97d59aa28e33dbe6a630599fcf240a2f5fddb84
lotus <daniel.younghwan.lee@gmail.com>
2022-05-12 14:07:30 -0400

    connected to api

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # weather-dashboard-challenge
 - In the process of setting up my files, I decided to link to Bootstrap to expedite any styling task. 
+- It was very important for me to get my retrieval from Local Storage to work properly (Local Storage has been my nemesis the past couple of challenges).
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,80 @@ console.log("hello world");
 
 var searchFormEl = document.querySelector("#search-form");
 var cityInputEl = document.querySelector("#city-name");
+var searchHistoryEl = document.querySelector("#search-history");
+var cityAndDateEl = document.querySelector("#city-and-date");
 
+var cityStorage = [];
+var today = moment().format("M/DD/YYYY");
+
+// button functionality
 var formSubmitHandler = function (event) {
     event.preventDefault();
     var cityName = cityInputEl.value.trim();
 
     if (cityName) {
-        console.log(cityName);
         cityInputEl.value = "";
+        saveCity(cityName);
     } else {
         alert("Please enter a city name");
     }
 };
 
+// Local Storage functions
+var saveCity = function(cityName) {
+    cityStorage.push(cityName);
+    localStorage.setItem("city", JSON.stringify(cityStorage));
+
+    displayCity(cityName);
+    getCityInfo(cityName);
+};
+
+var loadCity = function() {
+    var storedCity = localStorage.getItem("city");
+    if (!storedCity) {
+        return false;
+    }
+    cityStorage = JSON.parse(storedCity);
+
+    for (var i = 0; i < cityStorage.length; i++) {
+        var cityName = cityStorage[i];
+        displayCity(cityName);
+    }
+};
+
+// search history
+var displayCity = function(cityName) {
+    var cityListEl = document.createElement("li"); // change to link element "a or button" eventually, and active city item should be highlighted
+    cityListEl.classList = "list-group-item";
+    cityListEl.textContent = cityName;
+    searchHistoryEl.appendChild(cityListEl);
+}
+
+// api request
+var getCityInfo = function(cityName) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=05bd4265f18fe414d9b3357776b08c4d"
+
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data)
+                displayWeather(data);
+            });
+        } else {
+            alert("Error: City not found");
+        }
+    })
+    .catch(function(error) {
+        alert("Unable to connect to GitHub");
+    });
+};
+
+// display weather
+displayWeather = function(data) {
+    var name = data.name;
+    cityAndDateEl.textContent = name + " (" + today + ")";
+}
+
 searchFormEl.addEventListener("submit", formSubmitHandler);
+
+loadCity();
