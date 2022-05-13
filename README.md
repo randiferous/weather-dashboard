36f8820c624b58a321247f44c8bd03febd46aa2d
lotus <daniel.younghwan.lee@gmail.com>
2022-05-13 01:02:15 -0400

    current weather complete

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 - In the process of setting up my files, I decided to link to Bootstrap to expedite any styling task. 
 - It was very important for me to get my retrieval from Local Storage to work properly (Local Storage has been my nemesis the past couple of challenges).
 - Making sure that the city button in search history properly displayed the weather was a hurdle. I learned that an event.target could be used to pass parameters.
+- Great "ah-ha" moment was figuring out how to display the weather icon
+- I got to use the Network tab in DevTools to figure out where to extract weather information
+- switching into imperial measurement for the temperature was a nice little hurdle
+- when I got to displaying the UV Index, the load time became longer. I suspect it because of the second fetch request.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
-console.log("hello world");
-
 var searchFormEl = document.querySelector("#search-form");
 var cityInputEl = document.querySelector("#city-name");
 var searchHistoryEl = document.querySelector("#search-history");
 var currentWeatherEl = document.querySelector("#current-weather");
 var cityAndDateEl = document.querySelector("#city-and-date");
+var weatherIconEl = document.querySelector("#weather-icon");
+var currentTempEl = document.querySelector("#current-temperature");
+var currentHumidEl = document.querySelector("#current-humidity");
+var currentWindEl = document.querySelector("#current-wind");
+var UvIndexEl = document.querySelector("#current-uv-index");
 
 var cityStorage = [];
 var today = moment().format("M/DD/YYYY");
@@ -23,7 +26,7 @@ var formSubmitHandler = function (event) {
 };
 
 // Local Storage functions
-var saveCity = function(cityName) {
+var saveCity = function (cityName) {
     cityStorage.push(cityName);
     localStorage.setItem("city", JSON.stringify(cityStorage));
 
@@ -31,7 +34,7 @@ var saveCity = function(cityName) {
     getCityInfo(cityName);
 };
 
-var loadCity = function() {
+var loadCity = function () {
     var storedCity = localStorage.getItem("city");
     if (!storedCity) {
         return false;
@@ -45,7 +48,7 @@ var loadCity = function() {
 };
 
 // search history
-var displayCity = function(cityName) {
+var displayCity = function (cityName) {
     var cityListEl = document.createElement("button");
     cityListEl.classList = "list-group-item list-group-item-action";
     cityListEl.setAttribute("type", "button");
@@ -55,40 +58,72 @@ var displayCity = function(cityName) {
     cityListEl.addEventListener("click", eventHandler);
 };
 
-var eventHandler = function(event) {
+var eventHandler = function (event) {
     getCityInfo(event.target.textContent);
 }
 
 // api request
-var getCityInfo = function(cityName) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=05bd4265f18fe414d9b3357776b08c4d"
+var getCityInfo = function (cityName) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=05bd4265f18fe414d9b3357776b08c4d";
 
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
-                // console.log(data)
                 displayWeather(data);
             });
         } else {
             alert("Error: City not found");
         }
     })
-    .catch(function(error) {
-        alert("Unable to connect to GitHub");
-    });
+        .catch(function (error) {
+            alert("Unable to connect to GitHub");
+        });
 };
 
 // display weather
-displayWeather = function(data) {
+displayWeather = function (data) {
     var name = data.name;
+    cityAndDateEl.textContent = name + " (" + today + ")";
 
     var icon = data.weather[0].icon;
     var iconUrl = "http://openweathermap.org/img/w/" + icon + ".png";
-    var iconImage = document.createElement("img");
-    iconImage.setAttribute("src", iconUrl);
-    currentWeatherEl.appendChild(iconImage);
-    cityAndDateEl.innerHTML = name + " (" + today + ")";
+    weatherIconEl.setAttribute("src", iconUrl);
+
+    var temperature = data.main.temp;
+    currentTempEl.textContent = "Temperature: " + temperature + " °F";
+
+    var humidity = data.main.humidity;
+    currentHumidEl.textContent = "Humidity: " + humidity + "%";
+
+    var windSpeed = data.wind.speed;
+    currentWindEl.textContent = "Wind Speed: " + windSpeed + " MPH";
+
+    displayUvIndex(data);
 }
+
+// display UV index
+displayUvIndex = function (data) {
+    var lat = data.coord.lat;
+    var lon = data.coord.lon;
+
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&appid=05bd4265f18fe414d9b3357776b08c4d";
+
+    fetch(apiUrl).then(function (response) {
+        response.json().then(function (data) {
+            var UvIndex = data.current.uvi;
+            UvIndexEl.textContent = "UV Index: " + UvIndex;
+
+            if (UvIndex < 3) {
+                UvIndexEl.className = "bg-success col-6 d-flex justify-content-end";
+            } else if (UvIndex > 7) {
+                UvIndexEl.className = "bg-danger col-6 d-flex justify-content-end";
+            } else {
+                UvIndexEl.className = "bg-warning col-6 d-flex justify-content-end";
+            }
+        });
+    });
+};
+
 
 searchFormEl.addEventListener("submit", formSubmitHandler);
 
